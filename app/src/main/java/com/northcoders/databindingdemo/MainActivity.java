4e6bf8126510b348f7bd154d0e8fb2e9e9a8e66b
ag-af <agabialves11@gmail.com>
2024-09-13 11:23:32 +0100

    feat: create Person class.

---
+++ app/src/main/java/com/northcoders/databindingdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.northcoders.databindingdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.northcoders.databindingdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        Person person = new Person("Simon", 18, "simon@northcoders.com");
+        activityMainBinding.setPerson(person);
+    }
+}
---
+++ app/src/main/java/com/northcoders/databindingdemo/Person.java
@@ -0,0 +1,37 @@
+package com.northcoders.databindingdemo;
+
+public class Person {
+    String name;
+    int age;
+    String email;
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
